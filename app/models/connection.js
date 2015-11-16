bd557cefce1037cb67c87e7f12f89097ff05ff7e
Cameron Wakal <cameron.wakal@gmail.com>
2015-11-15 18:08:06 -0700

    more css

--- app/models/connection.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  signalType: DS.attr('string'), //trigger or signal
-  sourceModule: DS.belongsTo('module'),
-  sourcePortName: DS.attr('string'),
-  destModule: DS.belongsTo('module'),
-  destPortName: DS.attr('string')
-});
