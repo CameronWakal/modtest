d77beba571d1326713422e3f34f08add13970eb5
Cameron Wakal <cameron.wakal@gmail.com>
2016-02-25 18:49:12 -0700

    rename array-value model to array-item

--- app/models/array.js
+++ app/models/array.js
@@ -3,15 +3,15 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   
   length: DS.attr('number'),
-  inputs: DS.hasMany('arrayValue'),
-  currentInput: DS.belongsTo('arrayValue', {async:false}),
+  inputs: DS.hasMany('arrayItem'),
+  currentInput: DS.belongsTo('arrayItem', {async:false}),
   module: DS.belongsTo('module', {async:false, polymorphic:true}),
 
   initInputs() {
     //init inputs
     var input;
     for(var i = 0; i < this.get('length'); i++) {
-      input = this.store.createRecord('arrayValue', {array:this, index:i});
+      input = this.store.createRecord('arrayItem', {array:this, index:i});
     }
   }
 
--- tests/unit/models/array-item-test.js
+++ tests/unit/models/array-item-test.js
@@ -1,6 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('array-value', 'Unit | Model | array value', {
+moduleForModel('array-item', 'Unit | Model | array item', {
   // Specify the other units that are required for this test.
   needs: []
 });
