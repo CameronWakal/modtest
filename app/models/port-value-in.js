bedf760feb1b389b5801326be2bde169dc49a227
Cameron Wakal <cameron.wakal@gmail.com>
2016-03-06 10:17:52 -0700

    configurable values for disabled value in ports

--- app/models/port-value-in.js
+++ app/models/port-value-in.js
@@ -6,8 +6,14 @@ export default Port.extend({
 
   //todo: get the _super model and the patch controller working with this being a belongsTo
   connections: DS.hasMany('port-value-out', {async: true}),
+  disabledValue: DS.attr('number'),
 
   getValue() {
+
+    if(!this.get('isEnabled')) {
+      return this.get('disabledValue');
+    }
+
     //sum all input values but leave the result as null if all inputs are null
     var value = null;
     var totalValue = null;
