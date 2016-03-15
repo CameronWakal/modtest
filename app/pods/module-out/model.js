0d0d6a0558b5edc10a961c06dcabb316bbb04059
Cameron Wakal <cameron.wakal@gmail.com>
2016-03-14 18:50:54 -0600

    event performance stats

--- app/pods/module-out/model.js
+++ app/pods/module-out/model.js
@@ -10,6 +10,8 @@ export default Module.extend({
 
   noteInPort: DS.belongsTo('port-value-in', {async:false}),
 
+  events: [],
+
   sendEvent(event) {
     //the clock adds some padding ms to the event timestamps to allow for callback latency.
     //send an alert if the callback latency is more than the allowed padding.
@@ -17,6 +19,37 @@ export default Module.extend({
     if(netLatency>0) {
         console.log('Note event is late by '+netLatency);
     }
+
+    // Diagnostic:
+    // Calculate average callback delay and average time for event to traverse graph.
+    
+    event.completionTime = window.performance.now();
+    this.get('events').push(event);
+      if(this.get('events.length') >= 64) {
+
+        let callbackDeltas = this.get('events').map(item=>{
+          return item.callbackTime - item.targetTime;
+        });
+        let executionDeltas = this.get('events').map(item=>{
+          return item.completionTime - item.callbackTime;
+        });
+
+        let callbackTotal = callbackDeltas.reduce(function(prev, item){
+          return prev + item;
+        });
+        let executionTotal = executionDeltas.reduce(function(prev, item){
+          return prev + item;
+        });
+
+        let callbackAverage = callbackTotal / callbackDeltas.length;
+        let executionAverage = executionTotal / executionDeltas.length;
+
+        console.log(callbackAverage, executionAverage);
+
+        this.set('events', []);
+
+      }
+      
 
     //check the connection of the 'note' port for the value of the note to play.
     //let notePort = this.get('ports').findBy('label', 'note');
