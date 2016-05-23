4fdb36556984dd775af791b404ba5ef6b2ea466d
Cameron Wakal <cameron.wakal@gmail.com>
2016-05-22 18:42:35 -0600

    added emberx-range-input
    fixed value initialization bug

--- app/components/value-input-fader.js
+++ app/components/value-input-fader.js
@@ -1,60 +1,40 @@
 import Ember from 'ember';
+import XRangeInputComponent from 'emberx-range-input/components/x-range-input';
 
 //fader depends on rangeslider.js so that the vertical slider can have custom styles
 //not possible with chrome html5 range input
 
-export default Ember.Component.extend({
-  tagName: 'input',
-  attributeBindings: ['type', 'data-orientation', 'value', 'min', 'max', 'step'],
-  type: 'range',
-  'data-orientation': 'vertical',
-  value: null,
-  classNames: ['value-input-fader'],
+export default XRangeInputComponent.extend({
 
-  onRangeUpdated: Ember.observer('min', 'max', 'step', function() {
-    console.log('range updated max', this.get('max'), 'attr max', this.attrs.max.value);
-    this.$().rangeslider('update', true);
-  }),
+  attributeBindings: ['data-orientation', 'noValue:disabled'],
+  'data-orientation': 'vertical',
+
+  noValue: Ember.computed.empty('value'),
 
   didInsertElement() {
-    console.log('did insert fader, value is', this.get('value'), 'max is', this.get('max'));
-    let self = this;
-    self.$().rangeslider({
-
-      // Feature detection the default is `true`.
-      // Set this to `false` if you want to use
-      // the polyfill also in Browsers which support
-      // the native <input type="range"> element.
-      polyfill: false,
-
-      // Default CSS classes
-      rangeClass: 'rangeslider',
-      disabledClass: 'rangeslider--disabled',
-      horizontalClass: 'rangeslider--horizontal',
-      verticalClass: 'rangeslider--vertical',
-      fillClass: 'rangeslider__fill',
-      handleClass: 'rangeslider__handle',
-      
-      // Callback function
-      onInit: function() {
-        console.log('slider init, value is', self.get('value'), 'element', self.$()[0], 'element value', self.$()[0].value, 'element max', self.$()[0].max);
-      },
-
-      // Callback function
-      onSlide: function(position, value) {
-        self.set('value', value);
-      },
-
-      // Callback function
-      onSlideEnd: function(position, value) {
-
-      }
-        
-    });
+    this._super();
+    if (this.get('type') === 'range') {
+        var self = this;
+        self.$().rangeslider({
+            polyfill: false,
+            onInit: function() {
+                self.sendAction('onInit');
+            },
+            onSlide: function(position, value) {
+                self.set('value', value);
+                self.sendAction('onSlide', position, value);
+            },
+            onSlideEnd: function(position, value) {
+                self.set('value', value);
+                self.sendAction('onSlideEnd', position, value);
+            },
+        });
+    }
   },
 
   willDestroyElement() {
     this.$().rangeslider('destroy');
+    this._super();
   }
 
 });
