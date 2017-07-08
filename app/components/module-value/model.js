60bbfe104983057dc929a40c9fa05e980cdd0147
CameronWakal <cameron.wakal@gmail.com>
2017-07-08 13:36:00 -0600

    improved button styles and value update port
    todo:
    - clean up button styles

--- app/components/module-value/model.js
+++ app/components/module-value/model.js
@@ -8,14 +8,19 @@ const {
 } = Ember;
 
 const {
-  attr
+  attr,
+  belongsTo
 } = DS;
+
+const latency = 10;
 
 export default Module.extend({
 
   type: 'module-value', // modelName that can be referenced in templates, constructor.modelName fails in Ember > 2.6
   label: 'Value',
   value: attr('number'),
+
+  changeOutPort: belongsTo('port-event-out', { async: false }),
 
   getValue() {
     return get(this, 'value');
@@ -25,6 +30,7 @@ export default Module.extend({
     if (get(this, 'isNew')) {
       // create ports
       this.addValueOutPort('value', 'getValue', true);
+      this.addEventOutPort('changed', 'changeOutPort', false);
       console.log('module-value didCreate saveLater');
       this.requestSave();
     }
@@ -33,6 +39,14 @@ export default Module.extend({
   onValueChanged: observer('value', function() {
 
     if (get(this, 'hasDirtyAttributes')) {
+
+      let changeEvent = {
+        targetTime: performance.now(),
+        outputTime: performance.now() + latency,
+        callbackTime: performance.now()
+      };
+      get(this, 'changeOutPort').sendEvent(changeEvent);
+
       console.log('module-value.onValueChanged() requestSave()');
       this.requestSave();
     }
