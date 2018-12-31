71ed551d6dec8e3809f175097be346a8b7e04ea1
CameronWakal <cameron.wakal@gmail.com>
2018-12-31 13:00:49 -0700

    ports rendering by group in template

--- app/components/port-group/model.js
+++ app/components/port-group/model.js
@@ -1,9 +1,11 @@
+import { filterBy } from '@ember/object/computed';
 import DS from 'ember-data';
 
 const { Model, hasMany } = DS;
 
 export default Model.extend({
   ports: hasMany('port', { polymorphic: true, async: false }),
+  enabledPorts: filterBy('ports', 'isEnabled', true),
 
   save() {
     this._super({ adapterOptions: { dontPersist: true } });
