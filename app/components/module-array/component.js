1a1ee80353dc3b527c3771d4c9558b45d5b704c4
CameronWakal <cameron.wakal@gmail.com>
2019-09-22 10:18:25 -0600

    completed array module

---
+++ app/components/module-array/component.js
@@ -0,0 +1,16 @@
+import { alias } from '@ember/object/computed';
+import { observer } from '@ember/object';
+import ModuleGenericComponent from '../module/component';
+
+export default ModuleGenericComponent.extend({
+
+  classNames: ['module-sequence'],
+
+  latestTriggerTime: alias('module.latestTriggerTime'),
+  triggerDuration: alias('module.triggerDuration'),
+
+  onLayoutChanged: observer('module.steps.length', 'module.inputType', 'module.displayScale', function() {
+    this.layoutChanged();
+  })
+
+});
