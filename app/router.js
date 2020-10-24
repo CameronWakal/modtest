60d2ec6456b370c80e164e85d8c4b447c655f45f
CameronWakal <cameron.wakal@gmail.com>
2020-10-24 12:00:43 -0600

    ember 3.14 upgrade

--- app/router.js
+++ app/router.js
@@ -1,15 +1,13 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
   this.route('patch', { path: '/:patch_id' });
 
   this.route('patches', function() {});
 });
-
-export default Router;
