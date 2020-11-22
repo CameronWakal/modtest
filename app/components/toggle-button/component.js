323bc7652b02c29129ec8e4c4d76677340f2e16d
CameronWakal <cameron.wakal@gmail.com>
2020-11-21 22:41:23 -0700

    toggle button es6 component

--- app/components/toggle-button/component.js
+++
@@ -1,8 +0,0 @@
-import Component from '@ember/component';
-
-export default Component.extend({
-  tagName: 'button',
-
-  classNames: ['toggle-button'],
-  classNameBindings: ['isOn:toggle-button-on']
-});
