f483122c71501539f95bc3263f7332709720b6c9
CameronWakal <cameron.wakal@gmail.com>
2020-11-21 22:06:41 -0700

    select-menu es6 component

--- app/components/module-setting-menu/component.js
+++ app/components/module-setting-menu/component.js
@@ -1,5 +1,13 @@
 import Component from '@ember/component';
+import { action, set } from '@ember/object';
 
 export default Component.extend({
-  classNames: ['module-setting', 'module-setting-menu']
+  classNames: ['module-setting', 'module-setting-menu'],
+
+  @action
+  settingChanged(event) {
+    let selectedIndex = event.target.selectedIndex;
+    set(this, 'setting.value', this.setting.items[selectedIndex]);
+  }
+
 });
--- app/components/select-menu/component.js
+++ app/components/select-menu/component.js
@@ -1,16 +1,3 @@
-import Component from '@ember/component';
-import { set, get } from '@ember/object';
+import Component from '@glimmer/component';
 
-// todo: generalize patch-menu and select-menu to be a single component
-export default Component.extend({
-
-  tagName: 'select',
-  attributeBindings: ['name'],
-
-  change() {
-    let { selectedIndex } = this.element;
-    let items = this.items;
-    set(this, 'selectedItem', items[selectedIndex]);
-  }
-
-});
+export default class SelectMenu extends Component {}
