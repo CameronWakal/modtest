0afefd745a471cac33e47509e73b1fed421e3403
CameronWakal <cameron.wakal@gmail.com>
2020-11-29 22:36:49 -0700

    clean up imports

--- app/components/value-input-number/component.js
+++ app/components/value-input-number/component.js
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
-import { set, get, computed, observer, action } from '@ember/object';
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 // from template:
